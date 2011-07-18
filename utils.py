83ea7d5adeed95ed98a4037a99945fca7f79dbf6
Griffith Rees <griff.rees@gmail.com>
2011-07-18 15:23:19 +0100

    Comment out bad import script and add city_fix function

--- utils.py
+++ utils.py
@@ -7,15 +7,26 @@ import os
 DATA_PATH = os.path.join(settings.PROJECT_PATH, 'area_codes', 'data')
 FILENAME = 'npanxx98.txt'
 
-def data_import(filename=FILENAME, path=DATA_PATH, delete=False):
-    if delete:
-        AreaCode.objects.all().delete()
+# This function is obsolete with new AreaCode Models
+#def data_import(filename=FILENAME, path=DATA_PATH, delete=False):
+#    
+#    if delete:
+#        AreaCode.objects.all().delete()
+#    file = os.path.join(path,filename)
+#    with open(file, 'r') as f:
+#        for s in f.readlines():
+#            l = s.split(' ')
+#            a = AreaCode(npa=l[0], nxx=l[1], latitude=l[2], longitude=l[3],
+#                    type=l[4], state=l[5], city=l[6])
+#            a.save()
+
+def city_fix(filename=FILENAME, path=DATA_PATH):
     file = os.path.join(path,filename)
     with open(file, 'r') as f:
         for s in f.readlines()[1:]:
             l = s.split(' ')
-            a = AreaCode(npa=l[0], nxx=l[1], latitude=l[2], longitude=l[3],
-                    type=l[4], state=l[5], city=l[6])
+            a = AreaCode.objects.get(npa=l[0], nxx=l[1], type=l[4], state=l[5])
+            a.city = ' '.join(l[6:])
             a.save()
 
 #def map_tracts(area_codes=AreaCode.us.filter(tract__isnull=True)):
